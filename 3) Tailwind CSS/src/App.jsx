9841274a8ae021dd0b02b767f8faadd901c6f806
CodewithNishchal <nischal100per@gmail.com>
2025-04-16 21:46:10 +0530

    Some changes

---
+++ 3) Tailwind CSS/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Cards from './Components/Cards'
+
+function App() {
+
+  return (
+    <>
+     <h1 className="bg-green-300 p-4 rounded-xl">Tailwind shit is here</h1>
+       <Cards />
+    </>
+  )
+}
+
+export default App
